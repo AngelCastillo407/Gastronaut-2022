f3223748b12581ad5a2e189a45b83d97091b220b
Angel Castillo <jrgongy@gmail.com>
2024-01-26 18:00:12 -0500

    Created new scene with space background + added tons of spheres (Asteroid stand-ins) into scene + added behavior where left mouse clicking flips camera towards player

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-
     public Transform player;
+    private bool facingBackwards = false;
+    private Vector3 newRelativeLocation;
+
+    private void Start()
+    {
+        newRelativeLocation = new Vector3(0, 1, -5);
+        transform.position = player.transform.position + newRelativeLocation;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        transform.position = player.position + newRelativeLocation;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            facingBackwards = !facingBackwards;
+            transform.eulerAngles += new Vector3(x: 0f, y: 180f, z: 0f);
+            newRelativeLocation = new Vector3(x: 0f, y: 1f, z: facingBackwards ? 5f : -5f);
+        }
+
     }
 }
